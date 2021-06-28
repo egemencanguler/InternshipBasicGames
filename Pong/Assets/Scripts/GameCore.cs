5625f8eea3d6878201d1d1be66f26ce7ea909a1c
OsmnZeki <62132800+OsmnZeki@users.noreply.github.com>
2021-06-28 19:22:39 +0300

    Pong

---
+++ Pong/Assets/Scripts/GameCore.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class GameCore : MonoBehaviour
+{
+    public static GameCore instance;
+    
+    [SerializeField] private GameObject player1, player2;
+    [SerializeField] private GameObject ball;
+    private int scoreLeft=0,scoreRight=0;
+    [SerializeField] private TextMeshProUGUI text;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject Get_Ball()
+    {
+        return ball;
+    }
+
+    public GameObject Get_Player1()
+    {
+        return player1;
+    }
+
+    public GameObject Get_Player2()
+    {
+        return player2;
+    }
+
+    public void GoScore(int direction)
+    {
+        ball.GetComponent<Ball>().ResetTheBall();
+        if (direction == 1)
+        {
+            scoreLeft++;
+        }
+        else if(direction == -1)
+        {
+            scoreRight++;
+        }
+        text.text = "" + scoreLeft + " - " + scoreRight;
+        
+    }
+}
---
+++ Pong/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] public float movementSpeed = 1;
+    [SerializeField] Rect allowedArea;
+    private Bounds bounds;
+    private void Awake()
+    {
+        bounds = GetComponent<Renderer>().bounds;
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Movement();
+        bounds = GetComponent<Renderer>().bounds;
+    }
+
+    void Movement()
+    {
+        float direction = Input.GetAxisRaw("Vertical");
+        Vector2 newPos = transform.position;
+        newPos.y += movementSpeed * direction * Time.deltaTime;
+        if (!allowedArea.Contains(newPos))
+        {
+            newPos = transform.position;
+        }
+        transform.position = newPos;
+    }
+    public Bounds Get_bounds()
+    {
+        return bounds;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawCube(transform.position, bounds.size);
+    }
+}
